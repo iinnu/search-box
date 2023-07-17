e5ef1e2dc6ce170944a59d855d630336d07309b3
Inhu Jo <inhudev@gmail.com>
2023-07-18 00:53:26 +0900

    feat : useClickOutside 구현

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,29 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import { SearchForm } from './SearchForm/SearchForm';
 import { SearchRecommend } from './SearchRecommend/SearchRecommend';
+import { useClickOutside } from '@/hooks/useClickOutside';
 
 export function Search() {
+  const ref = useRef(null);
   const [keyword, setKeyword] = useState('');
+  const [isRecommendVisible, setIsRecommendVisible] = useState(false);
 
   const handleKeywordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setKeyword(value);
   };
 
+  const toggleRecommendVisible = () => setIsRecommendVisible(true);
+
+  useClickOutside(ref, () => {
+    setIsRecommendVisible(false);
+  });
+
   return (
-    <div>
-      <SearchForm keyword={keyword} onChange={handleKeywordChange} />
-      <SearchRecommend keyword={keyword} />
+    <div ref={ref}>
+      <SearchForm keyword={keyword} onChange={handleKeywordChange} onFocus={toggleRecommendVisible} />
+      {isRecommendVisible && <SearchRecommend keyword={keyword} />}
     </div>
   );
 }
--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -5,14 +5,15 @@ import { Button, Input, InputBase, InputWithIcon } from './SearchForm.style';
 interface SearchFormProps {
   keyword: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus: () => void;
 }
-export function SearchForm({ keyword, onChange }: SearchFormProps) {
+export function SearchForm({ keyword, onChange, onFocus }: SearchFormProps) {
   return (
     <form onSubmit={(event) => event.preventDefault()}>
       <InputBase>
         <InputWithIcon>
           <FaSearch />
-          <Input placeholder="질환명을 입력해주세요." value={keyword} onChange={onChange} />
+          <Input placeholder="질환명을 입력해주세요." value={keyword} onChange={onChange} onFocus={onFocus} />
         </InputWithIcon>
         <Button>검색</Button>
       </InputBase>
