66001904fdb26fdbef59bfe5a568eda223e214fa
Inhu Jo <inhudev@gmail.com>
2023-07-18 19:06:46 +0900

    feat : API 요청시 console.info 추가

--- src/api/search.ts
+++ src/api/search.ts
@@ -5,6 +5,8 @@ import { Sick } from '@/types';
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export async function getRecommendedKeywords(keyword: string): Promise<Sick[]> {
+  console.info('calling api');
+
   const response: AxiosResponse<Sick[]> = await axios.get(`${BASE_URL}?q=${keyword}`);
   response.data.splice(8);
   return response.data;
